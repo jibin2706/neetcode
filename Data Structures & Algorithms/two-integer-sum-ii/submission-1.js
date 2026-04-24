3300df4e06f956cd3676dd63825f953b2b2feafe
Jibin Thomas <jibin.thomas2706@gmail.com>
2026-04-24 14:57:11 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        const n = numbers.length;
+        for (let i = 0; i < n; i++) {
+            for (let j = i + 1; j < n; j++) {
+                if (numbers[i] + numbers[j] > target) break;
+                if (numbers[i] + numbers[j] === target) return [i + 1, j + 1];
+            }
+        }
+    }
+}
